3546fa29d9590e200dd102ff39ecbeec0b560566
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-06-02 00:01:05 +0900

    npm study

---
+++ npm_study/README.md
@@ -0,0 +1,5 @@
+## node_modules의 절대 경로
+
+- npm root
+- npm root -g // 전역
+- npm ls -g // 전역 모듈에 뭐뭐가 깔려있는지 확인
---
+++ npm_study/index.js
@@ -0,0 +1,5 @@
+const dayjs = require("dayjs");
+
+console.log(dayjs().format("YYYYMMDD"));
+
+console.log("hey");
