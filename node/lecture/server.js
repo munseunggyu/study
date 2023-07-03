f9c0f18f46570c77d65a179fa959360571ff8bd2
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-07-03 23:46:43 +0900

    server에서 html 읽은 후 보여주기

---
+++ node/lecture/server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs").promises;
+
+const server = http
+  .createServer(async (req, res) => {
+    try {
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      const data = await fs.readFile("./server.html");
+      res.end(data);
+    } catch (error) {
+      console.log(error);
+      // res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    }
+  })
+  .listen(8080);
+server.on("listening", () => {
+  console.log(`8080 port start...`);
+});
+server.on("error", (error) => {
+  console.log(error);
+});
